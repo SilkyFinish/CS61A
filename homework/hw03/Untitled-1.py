fc6f371d14e127f250772e23ce680005803cb822
SilkyFinish <3259731417@qq.com>
2024-09-20 09:41:20 +0800

    提交文件

---
+++ homework/hw03/Untitled-1.py
@@ -0,0 +1,37 @@
+def num_eights(n):
+    """Returns the number of times 8 appears as a digit of n.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> num_eights(8782089)
+    3
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    total=0
+    if n<10:
+        if n==8:
+            return total+1
+        else:
+            return total+0
+    else:
+        remain,last=n//10,n%10
+        if last==8:
+            total+=1
+    return num_eights(remain)
+
+num_eights(88888)
